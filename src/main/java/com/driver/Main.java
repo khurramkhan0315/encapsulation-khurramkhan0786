5a8bdb565fb3463c00f8a450fd3fea6b9c8c3043
Khurram Khan <khankhurram2427@gmail.com>
2024-11-20 18:22:02 +0530

    Update Main.java
    some changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+   public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        rw.setId(123);  //due to private method its create an error
+        rw.setName("khurram");
+        System.out.println(rw.getId());
+        System.out.println(rw.getName());
+      }
 }
