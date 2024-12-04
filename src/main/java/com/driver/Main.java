ea853e1cf22c40e1393c92298efd93a6318cbb51
Khurram Khan <khankhurram2427@gmail.com>
2024-12-04 18:42:20 +0000

    complete

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        //rw.name = "khurram"
+        //'name' has private access in 'com. driver. RWOnly'
+        rw.setId(123);
+        rw.setName("khurram");
+        System.out.println(rw.getId());
+        System.out.println(rw.getName());
+    }
 }
